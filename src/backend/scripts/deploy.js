99694cf6dcda3815c118fe2641df1b4c625e6804
Rehan Ganapathy <79349712+rehanganapathy@users.noreply.github.com>
2022-04-18 21:54:00 +0530

    Add files via upload

---
+++ src/backend/scripts/deploy.js
@@ -0,0 +1,57 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+
+  const toWei = (num) => ethers.utils.parseEther(num.toString())
+  let royaltyFee = toWei(0.01);
+  let prices = [toWei(1), toWei(2), toWei(3), toWei(4), toWei(5), toWei(6), toWei(7), toWei(8)]
+  let deploymentFees = toWei(prices.length * 0.01)
+
+
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  // deploy contracts here:
+  const NFTMarketplaceFactory = await ethers.getContractFactory("MusicNFTMarketplace");
+  nftMarketplace = await NFTMarketplaceFactory.deploy(
+    royaltyFee,
+    artist.address,
+    prices,
+    { value: deploymentFees }
+  );
+
+  console.log("Smart Contract Address: ", nftmarketplace.address);
+
+  // For each contract, pass the deployed contract and name to this function to save a copy of the contract ABI and address to the front end.
+  saveFrontendFiles(nftmarketplace, "MusicNFTMArketPLace");
+}
+
+function saveFrontendFiles(contract, name) {
+  const fs = require("fs");
+  const contractsDir = __dirname + "/../../frontend/contractsData";
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  fs.writeFileSync(
+    contractsDir + `/${name}-address.json`,
+    JSON.stringify({ address: contract.address }, undefined, 2)
+  );
+
+  const contractArtifact = artifacts.readArtifactSync(name);
+
+  fs.writeFileSync(
+    contractsDir + `/${name}.json`,
+    JSON.stringify(contractArtifact, null, 2)
+  );
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
